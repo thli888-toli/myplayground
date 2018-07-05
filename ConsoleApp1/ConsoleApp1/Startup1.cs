e8a390ee5478ae2d038270cfd6724a2cfb0ee29f
Tom Li <toli@microsoft.com>
2018-07-05 17:43:33 +0800

    Initial Commit for ConselApp1

---
+++ ConsoleApp1/ConsoleApp1/Startup1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ConsoleApp1.Startup1))]
+
+namespace ConsoleApp1
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseErrorPage();
+            app.Run(context =>
+            {
+                if (context.Request.Path.Equals(new PathString("/fail")))
+                {
+                    throw new Exception("Random exception");
+                }
+
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello, world.");
+            });
+
+        }
+    }
+}
